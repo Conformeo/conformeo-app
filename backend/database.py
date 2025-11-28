521027f7956d0ae84def76ffec6b9ea0193e10a5
Michel Germanotti <contact@conformeo-app.fr>
2025-11-28 12:30:16 +0100

    Préparation déploiement : Dockerfile et Database dynamique

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,21 @@
-# Fichier: backend/database.py
+import os # <--- On importe le module système
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL de connexion (correspond aux infos du docker-compose)
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:password123@localhost:5433/conformeo_db"
+# URL par défaut (Local) vs URL Cloud (Récupérée via os.getenv)
+# Render nous donnera une URL qui commence par "postgres://", mais SQLAlchemy veut "postgresql://"
+database_url = os.getenv("DATABASE_URL", "postgresql://admin:password123@localhost:5433/conformeo_db")
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URL = database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Fonction utilitaire pour récupérer la session DB dans chaque requête
 def get_db():
     db = SessionLocal()
     try:
