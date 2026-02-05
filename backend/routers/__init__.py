d88910f995dbd37be591dae31f2b5db3ca8fd5be
Michel Germanotti <contact@conformeo-app.fr>
2026-02-05 13:31:18 +0100

    fix init

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,8 +1,8 @@
-from . import users
-from . import duerp
-from . import auth
-from . import companies
-from . import chantiers
-from . import tasks
-from . import dashboard
-from . import materiels
+# from . import users
+# from . import duerp
+# from . import auth
+# from . import companies
+# from . import chantiers
+# from . import tasks
+# from . import dashboard
+# from . import materiels
